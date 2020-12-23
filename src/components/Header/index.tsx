bab8cf7b25b776013e26bc30fa4774d470c8d856
carlosgomes1 <carlos_gomes.s@hotmail.com>
2020-12-23 05:03:24 -0300

    Header nav

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,9 +1,26 @@
 import React from 'react';
 
-// import { Container } from './styles';
+import { Container, Content, Logo, Nav, NavItem, Separator } from './styles';
+
+import LogoSVG from '../../assets/images/politician.svg';
 
 const Header: React.FC = () => {
-  return <div />;
+  return (
+    <Container>
+      <Content>
+        <Logo src={LogoSVG} />
+        <Nav>
+          <NavItem to="">Deputados</NavItem>
+          <Separator />
+          <NavItem to="">Partidos</NavItem>
+          <Separator />
+          <NavItem to="">Eventos</NavItem>
+          <Separator />
+          <NavItem to="">Frentes</NavItem>
+        </Nav>
+      </Content>
+    </Container>
+  );
 }
 
 export default Header;
